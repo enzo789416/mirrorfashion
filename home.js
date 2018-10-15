f66bee365d75b62028dd0666d5be35d639d8d25e
enzo789416 <41880119+enzo789416@users.noreply.github.com>
2018-10-15 17:25:58 -0300

    mirrorfashion
    Falta somente o php

---
+++ home.js
@@ -0,0 +1,6 @@
+$('.novidades	button').click(function() {
+	$('.novidades').addClass('painel-aberto');
+});
+$('.novidades	button').click(function() {
+	$('.novidades').toggleClass('painel-aberto');
+});
